935fdcd9e3274b9e6cf62c63fd1cb968ce11d49a
Edward Cluster <ecluster@csumb.edu>
2021-09-02 23:34:51 -0700

    Landing page created and connected to the login page

--- app/src/main/java/com/example/restapiproject/MainActivity.java
+++ app/src/main/java/com/example/restapiproject/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.restapiproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etUsername;
+    EditText etPassword;
+    View btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnLogin) {
+            Intent i = new Intent(this, LandingActivity.class);
+            startActivity(i);
+        }
     }
 }
